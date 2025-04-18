8127b3260e962270ec7bfee7aed517553469251a
asmsulaiman89 <asmsulaiman89@gmail.com>
2025-04-18 11:21:50 +0600

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,81 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+
+  //fetch all product
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  //add to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to cart");
+  };
+
+  //update cart Item Quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart updated");
+  };
+
+  //Remove Product from Cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Removed from cart");
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
---
+++ client/src/pages/AllProducts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAppContext } from "../context/AppContext";
+
+const AllProducts = () => {
+  const { products } = useAppContext();
+  return (
+    <div className="mt-16 flex flex-col">
+      <div className="flex flex-col items-end w-max">
+        <p className="text-2xl uppercase font-medium">All Products</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
