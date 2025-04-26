5428d424d1665a7aaf94b1fa244194134d42f966
sulaiman-00 <asmsulaiman00@gmail.com>
2025-04-26 12:43:31 +0600

    allowed all origin

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 //allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+// const allowedOrigins = ["http://localhost:5173"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
   })
 );
