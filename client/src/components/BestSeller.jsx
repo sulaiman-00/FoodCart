3639dd994fb80829f97b0d6f5352130b20847eea
sulaiman-00 <asmsulaiman00@gmail.com>
2025-04-26 21:49:53 +0600

    css changes

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,7 +7,7 @@ const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+      <div className="grid grid-cols-1   md:grid-cols-2 gap-3 lg:grid-cols-3 md:gap-6 2xl:grid-cols-5 mt-6">
         {products
           .filter((product) => product.inStock)
           .slice(0, 5)
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -131,7 +131,7 @@ const Navbar = () => {
         <div
           className={`${
             open ? "flex" : "hidden"
-          } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+          } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-10`}
         >
           <NavLink to="/" onClick={() => setOpen(false)}>
             Home
